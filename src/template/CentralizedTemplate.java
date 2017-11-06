efa023a04e67d507f91bf9e5dd4754af7fc26ece
Pierre Vigier <pierre.vigier@ymail.com>
2017-11-06 23:31:48 +0100

    Clean code

--- src/template/CentralizedTemplate.java
+++ src/template/CentralizedTemplate.java
@@ -20,222 +20,166 @@ import logist.task.TaskDistribution;
 import logist.task.TaskSet;
 import logist.topology.Topology;
 
-
 public class CentralizedTemplate implements CentralizedBehavior {
 
-    private Topology topology;
-    private TaskDistribution distribution;
-    private Agent agent;
-    private int nbTasks;
-    private long timeout_setup;
-    private long timeout_plan;
-
-    @Override
-    public void setup(Topology topology, TaskDistribution distribution,
-                      Agent agent) {
-
-        // this code is used to get the timeouts
-        LogistSettings ls = null;
-        try {
-            ls = Parsers.parseSettings("config" + File.separator + "settings_default.xml");
-        }
-        catch (Exception exc) {
-            System.out.println("There was a problem loading the configuration file.");
-        }
-
-        // the setup method cannot last more than timeout_setup milliseconds
-        timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
-        // the plan method cannot execute more than timeout_plan milliseconds
-        timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
-
-        this.topology = topology;
-        this.distribution = distribution;
-        this.agent = agent;
-    }
-
-    @Override
-    public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
-        long tic = System.currentTimeMillis();
-
-        final double prob = 0.5;
-
-        this.nbTasks = tasks.size();
-
-        Vehicle[] vArray = new Vehicle[vehicles.size()];
-        vArray = vehicles.toArray(vArray);
-
-        Random r = new Random();
-
-        MyPlan initialPlan = initialPlan(vArray, tasks); // Give all the tasks to the first vehicle
-
-        MyPlan currentPlan = initialPlan;
-
-        MyPlan finalPlan = currentPlan;
-
-        currentPlan.print();
-
-        int nbIterations = 0;
-        while(nbIterations < 1000){
-            System.out.println("Iteration " + (nbIterations + 1));
-
-
-            List<MyPlan> possibleNeighbours = new ArrayList<MyPlan>();
-
-            possibleNeighbours.addAll(interChanges(currentPlan));
-            //System.out.println("Inter done");
-            possibleNeighbours.addAll(intraChanges(currentPlan));
-            //System.out.println("Intra done");
-
-
-
-            MyPlan bestNeighbor = null;
-            double minCost = Double.MAX_VALUE;
-
-
-            for(MyPlan neighbor : possibleNeighbours) {
-                if(neighbor.isValid(vArray, nbTasks)) {
-                    double cost = neighbor.computePlanCost(vArray);
-                    if (cost<minCost){
-                        bestNeighbor = neighbor;
-                        minCost = cost;
-                    }
-                }
-            }
-
-            double randomNumber = r.nextDouble();
-            double currentCost = currentPlan.computePlanCost(vArray);
-
-            if (minCost<currentCost || randomNumber>prob){
-                currentPlan = bestNeighbor;
-            }
-
-
-
-            System.out.println("Cost [" + currentCost + "]");
-
-            if(currentPlan.computePlanCost(vArray) < finalPlan.computePlanCost(vArray)){
-                finalPlan = currentPlan;
-            }
-
-            randomNumber = r.nextDouble();
-
-            if (randomNumber>0.99){
-                currentPlan = finalPlan;
-            }
-            nbIterations++;
-        }
-
-
-
-        // Print the final possiblePlan.
-        System.out.println("Costs : [" + finalPlan.computePlanCost(vArray) + "]");
-        finalPlan.print();
-
-        long toc = System.currentTimeMillis();
-        long duration = toc - tic;
-        System.out.println("Computing duration : " + duration + " milliseconds.");
-
-        // Compare with the naive solution
-        System.out.println("Naive cost : " + initialPlan.computePlanCost(vArray));
-
-        List<Plan> plans = new LinkedList<Plan>();
-
-        for (Vehicle vehicle : vehicles) {
-            plans.add(finalPlan.generatePlan(vehicle));
-        }
-
-        return plans;
-    }
-
-
-
-    private MyPlan initialPlan(Vehicle[] vehicles, TaskSet tasks) {
-
-        MyPlan s = new MyPlan(vehicles.length);
-
-        for(Task t : tasks) {
-            s.addTask(vehicles[0].id(), t);
-        }
-
-        return s;
-    }
-
-    private MyPlan naivePlan(Vehicle vehicle, TaskSet tasks) {
-        MyPlan s = new MyPlan(1);
-        int vehicleId = vehicle.id();
-
-        for(Task task : tasks) {
-            s.addTask(vehicleId, task);
-            //s.addTask(vehicleId, delivery);
-        }
-
-        return s;
-    }
-
-
-
-    private Set<MyPlan> interChanges(MyPlan currentPlan) {
-        //currentPlan.print();
-        Random r = new Random();
-
-        Set<MyPlan> neighbors = new HashSet<MyPlan>();
-
-        int nbVehicles = currentPlan.getNbVehicles();
-        //System.out.println(nbVehicles);
-
-        for (int vehicleId = 0; vehicleId<currentPlan.getNbVehicles();vehicleId++){
-
-        //int vehicleId = r.nextInt(currentPlan.getNbVehicles()-1);
-            int nbTasksVehicle = currentPlan.getNbTasksVehicle(vehicleId);
-
-            for (int i = 0; i < nbTasksVehicle-1; i++) { // For each of the vehicle tasks
-                for (int recieverVehicle = 0; recieverVehicle < currentPlan.getNbVehicles(); recieverVehicle++) {
-                    if(vehicleId != recieverVehicle) {
-                        MyPlan neighbor = currentPlan.clone();
-                        //neighbor.print();
-                        neighbor = neighbor.interSwapTasks(vehicleId, recieverVehicle, i);
-                        neighbors.addAll(intraChanges(neighbor));
-                    }
-                }
-            }
-
-        }
-
-
-        return neighbors;
-    }
-
-
-    private Set<MyPlan> intraChanges(MyPlan currentPlan) {
-
-        Random r = new Random();
-
-        int i;
-        int j;
-
-        Set<MyPlan> neighbors = new HashSet<MyPlan>();
-
-        int nbVehicles = currentPlan.getNbVehicles();
-
-        for (int vehicleId = 0; vehicleId<currentPlan.getNbVehicles();vehicleId++) {
-
-            int length = currentPlan.getNbTasksVehicle(vehicleId);
-
-
-            if (length >= 2) {
-                int switchNumber;
-                switchNumber = r.nextInt(length);
-                for (int sw = 0; sw < switchNumber; sw++) {
-                    i = r.nextInt(length);
-                    j = r.nextInt(length);
-                    MyPlan neighbor = currentPlan.intraSwapTasks(vehicleId, i, j);
-                    neighbors.add(neighbor);
-
-                }
-
-            }
-        }
-
-        return neighbors;
-    }
+	private Topology topology;
+	private TaskDistribution distribution;
+	private Agent agent;
+	private int nbTasks;
+	private long timeout_setup;
+	private long timeout_plan;
+
+	@Override
+	public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
+
+		// this code is used to get the timeouts
+		LogistSettings ls = null;
+		try {
+			ls = Parsers.parseSettings("config" + File.separator + "settings_default.xml");
+		} catch (Exception exc) {
+			System.out.println("There was a problem loading the configuration file.");
+		}
+
+		// the setup method cannot last more than timeout_setup milliseconds
+		timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
+		// the plan method cannot execute more than timeout_plan milliseconds
+		timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
+
+		this.topology = topology;
+		this.distribution = distribution;
+		this.agent = agent;
+	}
+
+	@Override
+	public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
+		// Initialization
+		long tic = System.currentTimeMillis();
+		final double prob = 0.5;
+		this.nbTasks = tasks.size();
+		Vehicle[] vArray = new Vehicle[vehicles.size()];
+		vArray = vehicles.toArray(vArray);
+		Random r = new Random();
+
+		// Give all the tasks to the vehicle with maximum capacity
+		MyPlan initialPlan = initialPlan(vArray, tasks);
+		MyPlan currentPlan = initialPlan;
+		MyPlan finalPlan = currentPlan;
+		currentPlan.print();
+
+		int nbIterations = 0;
+		while (nbIterations < 1000) {
+			System.out.println("Iteration " + (nbIterations + 1));
+
+			// Generate neighbors
+			List<MyPlan> possibleNeighbors = new ArrayList<MyPlan>();
+			possibleNeighbors.addAll(interChanges(currentPlan));
+			possibleNeighbors.addAll(intraChanges(currentPlan));
+
+			// Find the best neighbor
+			MyPlan bestNeighbor = null;
+			double minCost = Double.MAX_VALUE;
+			for (MyPlan neighbor : possibleNeighbors) {
+				if (neighbor.isValid(vArray, nbTasks)) {
+					double cost = neighbor.computePlanCost(vArray);
+					if (cost < minCost) {
+						bestNeighbor = neighbor;
+						minCost = cost;
+					}
+				}
+			}
+
+			// Choose the next plan
+			double randomNumber = r.nextDouble();
+			double currentCost = currentPlan.computePlanCost(vArray);
+			if (minCost < currentCost || randomNumber > prob)
+				currentPlan = bestNeighbor;
+
+			System.out.println("Cost [" + currentCost + "]");
+
+			// Update the best plan
+			if (currentPlan.computePlanCost(vArray) < finalPlan.computePlanCost(vArray)) {
+				finalPlan = currentPlan;
+			}
+
+			// Return the best plan
+			if (r.nextDouble() > 0.99)
+				currentPlan = finalPlan;
+			nbIterations++;
+		}
+
+		// Print the final possiblePlan.
+		System.out.println("Costs : [" + finalPlan.computePlanCost(vArray) + "]");
+		finalPlan.print();
+
+		long toc = System.currentTimeMillis();
+		long duration = toc - tic;
+		System.out.println("Computing duration : " + duration + " milliseconds.");
+
+		// Compare with the naive solution
+		System.out.println("Naive cost : " + initialPlan.computePlanCost(vArray));
+
+		List<Plan> plans = new LinkedList<Plan>();
+
+		for (Vehicle vehicle : vehicles) {
+			plans.add(finalPlan.generatePlan(vehicle));
+		}
+
+		return plans;
+	}
+
+	private MyPlan initialPlan(Vehicle[] vehicles, TaskSet tasks) {
+		// Look for the vehicle with the maximum capacity
+		Vehicle maxVehicle = vehicles[0];
+		for (Vehicle vehicle : vehicles) {
+			if (vehicle.capacity() > maxVehicle.capacity())
+				maxVehicle = vehicle;
+		}
+		MyPlan s = new MyPlan(vehicles.length);
+		// Give all the tasks to this vehicle
+		for (Task t : tasks)
+			s.addTask(maxVehicle.id(), t);
+		return s;
+	}
+
+	private Set<MyPlan> interChanges(MyPlan currentPlan) {
+		// Swap tasks between different vehicles
+		Random r = new Random();
+		Set<MyPlan> neighbors = new HashSet<MyPlan>();
+		for (int vehicleId = 0; vehicleId < currentPlan.getNbVehicles(); vehicleId++) {
+			int nbTasksVehicle = currentPlan.getNbTasksVehicle(vehicleId);
+			for (int i = 0; i < nbTasksVehicle - 1; i++) { // For each of the
+															// vehicle tasks
+				for (int receiverVehicle = 0; receiverVehicle < currentPlan.getNbVehicles(); receiverVehicle++) {
+					if (vehicleId != receiverVehicle) {
+						MyPlan neighbor = currentPlan.clone();
+						neighbor = neighbor.interSwapTasks(vehicleId, receiverVehicle, i);
+						neighbors.addAll(intraChanges(neighbor));
+					}
+				}
+			}
+		}
+
+		return neighbors;
+	}
+
+	private Set<MyPlan> intraChanges(MyPlan currentPlan) {
+		// Change the order of actions of each vehicles
+		Random r = new Random();
+		int i, j;
+		Set<MyPlan> neighbors = new HashSet<MyPlan>();
+		for (int vehicleId = 0; vehicleId < currentPlan.getNbVehicles(); vehicleId++) {
+			int length = currentPlan.getNbTasksVehicle(vehicleId);
+			if (length >= 2) {
+				int switchNumber;
+				switchNumber = r.nextInt(length);
+				for (int sw = 0; sw < switchNumber; sw++) {
+					i = r.nextInt(length);
+					j = r.nextInt(length);
+					MyPlan neighbor = currentPlan.intraSwapTasks(vehicleId, i, j);
+					neighbors.add(neighbor);
+				}
+			}
+		}
+		return neighbors;
+	}
 }
--- src/template/MyTask.java
+++ src/template/MyTask.java
@@ -4,45 +4,37 @@ import logist.task.Task;
 
 public class MyTask {
 
-    private Task task;
+	private Task task;
 
-    private boolean pickup; // Is it the pickup or the delivery of the task at stake ?
+	private boolean pickup; // Is it the pickup or the delivery of the task at
+							// stake ?
 
+	public MyTask(Task task, boolean pickup) {
+		this.task = task;
+		this.pickup = pickup;
+	}
 
-    public MyTask(Task task, boolean pickup) {
-        this.task = task;
-        this.pickup = pickup;
-    }
+	public Task getTask() {
+		return task;
+	}
 
+	public boolean isPickup() {
+		return pickup;
+	}
 
-    public Task getTask() {
-        return task;
-    }
+	public int getId() {
+		if (pickup)
+			return 2 * task.id + 1;
+		else
+			return 2 * task.id;
+	}
 
-
-
-    public boolean isPickup() {
-        return pickup;
-    }
-
-    public int getId(){
-        if (pickup){
-            return 2*task.id+1;
-        } else {
-            return 2*task.id;
-        }
-
-    }
-
-
-    @Override
-    public String toString() {
-        if(pickup){
-            return "Pickup : " + task.toString();
-        }
-        else{
-            return "Deliver : " + task.toString();
-        }
-    }
+	@Override
+	public String toString() {
+		if (pickup)
+			return "Pickup : " + task.toString();
+		else
+			return "Deliver : " + task.toString();
+	}
 
 }
